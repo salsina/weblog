962e9fed285cdd46d5d46439e9c21841de7bf4c4
Sina Salimian <50830157+salsina@users.noreply.github.com>
2022-03-26 22:39:15 +0430

    Add files via upload

---
+++ CodeYad-Blog.CoreLayer/DTOs/Users/UserFilterDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CodeYad_Blog.CoreLayer.Utilities;
+
+namespace CodeYad_Blog.CoreLayer.DTOs.Users
+{
+    public class UserFilterDto:BasePagination
+    {
+        public List<UserDto> Users { get; set; }
+    }
+
+}
