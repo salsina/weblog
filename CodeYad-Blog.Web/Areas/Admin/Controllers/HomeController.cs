4bf53c18a562e167da240486ba54fd646b137c31
Sina Salimian <50830157+salsina@users.noreply.github.com>
2022-03-26 22:44:55 +0430

    Add files via upload

---
+++ CodeYad-Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeYad_Blog.Web.Areas.Admin.Controllers
+{
+    public class HomeController : AdminControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
